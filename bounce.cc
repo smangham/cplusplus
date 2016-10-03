7c0332ca12ef0ffc196df8e221adbbafde9a9506
Sam Mangham <mangham@gmail.com>
2016-10-03 12:33:42 +0200

    Initial code

---
+++ bounce.cc
@@ -0,0 +1,29 @@
+// N.B. This program contains a bug, on purpose.
+
+#include <iostream>
+
+int main() {
+
+  char particleSymbol = 'x';
+  double particlePosition = 0;
+  double particleSpeed = 6.3;
+  int maxColumn = 80;
+  int timeStep = 0;
+  int stopTime = 60;
+
+  while (timeStep < stopTime) {
+    for (int i = 0; i < particlePosition; i++) {
+      std::cout << " ";
+    }
+    std::cout << particleSymbol << std::endl;
+    particlePosition += particleSpeed;
+    if (particlePosition >= maxColumn) {
+      particlePosition = maxColumn;
+      particleSpeed = -particleSpeed;
+    } else if (particleSpeed < 0) {
+      particlePosition = 0;
+      particleSpeed = -particleSpeed;
+    }    
+    timeStep++;
+  }
+}
