018dd71e0c5eefca388fc911f01eeb46bcfc0a00
Sam Mangham <mangham@gmail.com>
2016-10-06 16:08:13 +0100

    Added read in from file

--- bounce.cc
+++ bounce.cc
@@ -1,21 +1,40 @@
 // N.B. This program contains a bug, on purpose.
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 #include "Particle.hh"
 #include "Screen.hh"
 
 int main() {
   int timeStep = 0;
   const int stopTime = 60;
-  const int num_particles = 3;
+  int num_particles;
+  double pos, vel;
+  char sym;
+  Particle* p;
   Screen screen(1+(maxColumn-minColumn));
-  Particle p[num_particles];
-  // = {Particle(0,  6.3, 'x'), Particle(79, -4.4, 'o'), Particle(50,  3.0, '+')}; Better declaration
-  p[0] = Particle(0,  6.3, 'x');
-  p[1] = Particle(79, -4.4, 'o');
-  p[2] = Particle(50,  3.0, '+');
 
-  Screen test_screen(9);
-  test_screen = screen;
+  //Particle P = {Particle(0,  6.3, 'x'), Particle(79, -4.4, 'o'), Particle(50,  3.0, '+')}; Better declaration
+  std::ifstream in("particles.csv");
+  if(!in) {
+    std::cerr << "Could not open file!" << std::endl;
+    return EXIT_FAILURE;
+  } else {
+    in >> num_particles;
+    std::cout << "Reading in " << num_particles << " particles..." << std::endl;
+    p = new Particle[num_particles];
+  }
+
+  for(int i=0; i<num_particles; i++) {
+    in >> pos >> vel >> sym;
+    if(in.good()) {
+      p[i] = Particle(pos, vel, sym);
+      std::cout << "(" << i+1 << "/" << num_particles << ") " << p[i] << std::endl;
+    } else { 
+      std::cerr << "Error!" << std::endl;
+      return EXIT_FAILURE;
+    } 
+  }
 
   while (timeStep < stopTime) {
     screen.clear();
@@ -27,6 +46,7 @@ int main() {
     screen.print();
     timeStep++;
   }
+  std::cout << "Listing " << num_particles << " particles:" << std::endl;
   for(int i=0; i<num_particles; i++) {
     std::cout << p[i] << std::endl;
   }
