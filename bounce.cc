6fb77bd65affd6ef3b3d40064e774dbc832a9384
Sam Mangham <mangham@gmail.com>
2016-10-06 16:26:09 +0100

    Added whiles

--- bounce.cc
+++ bounce.cc
@@ -8,25 +8,34 @@
 int main() {
   int timeStep = 0;
   const int stopTime = 60;
-  int num_particles;
+  int num_particles = -1;
   double pos, vel;
   char sym;
   Particle* p;
   Screen screen(1+(maxColumn-minColumn));
 
   //Particle P = {Particle(0,  6.3, 'x'), Particle(79, -4.4, 'o'), Particle(50,  3.0, '+')}; Better declaration
-  std::ifstream in("particles.csv");
+  std::ifstream in("particles.dat");
   if(!in) {
     std::cerr << "Could not open file!" << std::endl;
     return EXIT_FAILURE;
   } else {
-    in >> num_particles;
-    std::cout << "Reading in " << num_particles << " particles..." << std::endl;
+    std::cout << "Reading";
+    while(!in.eof()) {
+      in >> sym >> pos >> vel;
+      std::cout << ".";
+      num_particles++;
+    }
     p = new Particle[num_particles];
+    in.close(); // Couldn't find reset
+    in.open("particles.dat");
   }
+
+  std::cout << " found " << num_particles << " particles." << std::endl;
 
   for(int i=0; i<num_particles; i++) {
     in >> pos >> vel >> sym;
+    std::cout << pos << vel << sym << std::endl;
     if(in.good()) {
       p[i] = Particle(pos, vel, sym);
       std::cout << "(" << i+1 << "/" << num_particles << ") " << p[i] << std::endl;
@@ -35,6 +44,8 @@ int main() {
       return EXIT_FAILURE;
     } 
   }
+
+  std::cout << "Read in " << num_particles << " particles." << std::endl;
 
   while (timeStep < stopTime) {
     screen.clear();
