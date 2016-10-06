e27ad9bc5bd9dcc9722241f8781ee1c910c1e5dd
Sam Mangham <mangham@gmail.com>
2016-10-06 12:29:45 +0100

    Moved functions outside of classes

--- bounce.cc
+++ bounce.cc
@@ -18,57 +18,54 @@ public:
     delete [] this->buffer;
   }
   //OPERATORS
-  friend void swap(Screen& one, Screen& two);
-  char& operator[]( const int pos ) {
-    if(pos >= 0 && pos < this->width && charvalid(this->buffer[pos])) {
-      return buffer[pos];
-    } else {
-      //Aha! We need more advanced C++ techniques here, or to not use this technique
-      return junk;
-    }
-  };
-  // Screen& operator=( const Screen& other) {
-  //   if(this != &other) {
-  //     if(this->width != other.width) {
-  //       delete [] this->buffer;
-  //       this->buffer = new char[other.width];
-  //       this->width = other.width;
-  //     }
-  //     std::copy(other.buffer, other.buffer+other.width, this->buffer);
-  //   }
-  //   return *this;
-  // }
-  Screen& operator=( Screen other) {
-    swap(*this, other);
-    //This lets us swap every member easily. 
-    //We just need a swap function for each type in the same scope the type is defined in 
-    return *this;
-  }
+  friend void swap(Screen&, Screen& );
+  char& operator[] ( const int );
+  Screen& operator=( Screen );
   //MEMBERS
-  // void draw( const int pos, const char sym ) {
-  //   if(pos >= 0 && pos < this->width && charvalid(this->buffer[pos])) {
-  //     this->buffer[pos] = sym;
-  //   }
-  // }
-  void clear() {
-    for (unsigned i = 0; i < this->width; i++) {
-      this->buffer[i] = ' ';
-    }  
-  }
-  void print() const {
-    for(int i=0; i< this->width; i++)
-    {
-      std::cout << this->buffer[i];
-    }
-    std::cout << std::endl;    
-  }
+  void clear();
+  void print() const;
 private:
   unsigned width;
   char *buffer, junk;
-  bool charvalid( const char test ) {
-    return( test == ' ' || test == '-');
-  }
+  bool charvalid( const char );
 };
+
+char& Screen::operator[]( const int pos ) {
+  if(pos >= 0 && pos < this->width && charvalid(this->buffer[pos])) {
+    return buffer[pos];
+  } else {
+    //Aha! We need more advanced C++ techniques here, or to not use this technique
+    return junk;
+  }
+}
+Screen& Screen::operator=( Screen other) {
+  swap(*this, other);
+  //This lets us swap every member easily. 
+  //We just need a swap function for each type in the same scope the type is defined in 
+  return *this;
+}
+//MEMBERS
+void Screen::clear() {
+  for (unsigned i = 0; i < this->width; i++) {
+    this->buffer[i] = ' ';
+  }  
+}
+void Screen::print() const {
+  for(int i=0; i< this->width; i++)
+  {
+    std::cout << this->buffer[i];
+  }
+  std::cout << std::endl;    
+}
+bool Screen::charvalid( const char test ) {
+  return( test == ' ' || test == '-');
+}
+
+void swap(Screen& one, Screen& two) {
+  using std::swap;
+  swap(one.width,  two.width);
+  swap(one.buffer, two.buffer);
+}
 
 class Particle {
 public:
@@ -79,48 +76,44 @@ public:
   // OPERATORS
   friend void swap(Particle& one, Particle& two);
   friend std::ostream& operator<<(std::ostream& os, const Particle &p);
-  Particle& operator=( Particle other ) {
-    swap(*this, other);
-    return (*this);
-  }
+  Particle& operator=( Particle );
   // MEMBERS
-  void move() {
-    this->pos += this->vel;
-
-    if (this->pos >= maxColumn) {
-      this->pos = maxColumn;
-      this->vel *= -1.0;
-    } else if (this->pos <= minColumn) {
-      this->pos = minColumn;
-      this->vel *= -1.0;
-    }     
-  }
-  void draw( Screen& screen ) const {
-    int ipos = static_cast<int>(this->pos);
-    //screen.draw(ipos, this->sym);
-    screen[ipos] = this->sym;
-    if(this->vel < 0.0) {
-      screen[ipos+2] = tail_short;
-      screen[ipos+1] = tail_long;
-      //screen.draw(ipos+1, tail_long);
-      //screen.draw(ipos+2, tail_short);
-    } else {
-      screen[ipos-2] = tail_short;
-      screen[ipos-1] = tail_long;
-      //screen.draw(ipos-1, tail_long);
-      //screen.draw(ipos-2, tail_short);
-    }
-  }
+  void move();
+  void draw( Screen& ) const;
 private:
   float pos, vel;
   char sym;
 };
 
-void swap(Screen& one, Screen& two) {
-  using std::swap;
-  swap(one.width,  two.width);
-  swap(one.buffer, two.buffer);
+void Particle::move() {
+  this->pos += this->vel;
+
+  if (this->pos >= maxColumn) {
+    this->pos = maxColumn;
+    this->vel *= -1.0;
+  } else if (this->pos <= minColumn) {
+    this->pos = minColumn;
+    this->vel *= -1.0;
+  }     
 }
+
+void Particle::draw( Screen& screen ) const {
+  int ipos = static_cast<int>(this->pos);
+  screen[ipos] = this->sym;
+  if(this->vel < 0.0) {
+    screen[ipos+2] = tail_short;
+    screen[ipos+1] = tail_long;
+  } else {
+    screen[ipos-2] = tail_short;
+    screen[ipos-1] = tail_long;
+  }
+}
+Particle& Particle::operator=( Particle other ) {
+  swap(*this, other);
+  return (*this);
+}
+
+
 void swap(Particle& one, Particle& two) {
   using std::swap;
   swap(one.sym, two.sym);
